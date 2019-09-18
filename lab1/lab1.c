fb44314f9c1f41ec6efb0977f4e5327f52854b36
group49 <group49@elec377.queensu.ca>
2019-09-18 11:22:20 -0400

    modifies lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -8,7 +8,18 @@
 #include <strings.h>
 #include <ctype.h>
 
-int isProcessDir(const struct dirent*d){
+int isProcessDir(const struct dirent*d) {
+	char *tmp = d->d_name;
+
+	while (tmp != '\0') {
+
+		if (isdigit(tmp) != 0) {
+		}	
+	}	
+	
+	if (isdigit(dirent->d_name) {
+			
+	dirent->d_name;
 }
 
 int main(){
