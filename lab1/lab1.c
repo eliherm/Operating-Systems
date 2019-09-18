ced14e56d005d6de74a80184a893855168189a16
Thomas Dean <trd@queensu.ca>
2019-09-18 11:28:57 -0400

    Initial Version of lab1

---
+++ lab1/lab1.c
@@ -0,0 +1,17 @@
+/*+
+ * lab1.c - Print process information
+-*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <strings.h>
+#include <ctype.h>
+
+int isProcessDir(const struct dirent*d){
+}
+
+int main(){
+   struct dirent ** namelist;
+
+}
