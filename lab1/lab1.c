bf19aa8d7ee8ffb888e98badd8514546f6ad6938
Elikem Hermon <eliherm@gmail.com>
2019-09-19 12:18:50 -0400

    Modifies lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -9,36 +9,38 @@
 #include <ctype.h>
 
 int isProcessDir(const struct dirent*d) {
-	char *tmp = d->d_name;
+    char *tmp = d->d_name;
 
-	while (*tmp != '\0') {
-		if (isdigit(*tmp) == 0) {
-			return 0;
-		}
-		tmp++;	
-	}
-	return 1;	
+    // Iterate until the end of the string
+    while (*tmp != '\0') {
+        // Check if the character is a digit
+        if (isdigit(*tmp) == 0)
+            return 0;
+        tmp++;
+    }
+    return 1;
 }
 
-int main(){
-   struct dirent ** namelist;
-   int numDirectories;
-   int i;
-   
-   numDirectories = scandir("/proc", &namelist, isProcessDir, NULL);
-   
-   // Check if no directories were read    
-   if (numDirectories == -1) {
-  	printf("The /proc directory could not be read"); 
-	return 1;
-   }
-   
-   for (i = 0; i < numDirectories; i++) {
-   		printf("%s \n", namelist[i]->d_name);
-   }
-   printf("\n");
-   
-   
-   return 0;
+void printDirectories(struct dirent **directoryList, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%s \n", directoryList[i]->d_name);
+    }
+    printf("\n");
+}
 
+int main() {
+    struct dirent ** namelist;
+    int numDirectories; // Stores the number of directories read by scandir
+
+    numDirectories = scandir("/proc", &namelist, isProcessDir, NULL);
+
+    // Check if no directories were read
+    if (numDirectories == -1) {
+        printf("The /proc directory could not be read");
+        return 1;
+    }
+
+    printDirectories(namelist, numDirectories);
+    return 0;
 }
