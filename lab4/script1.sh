b59b409aeb4824883e5ddb79041f2d2b5a0953c2
Elikem Hermon <eliherm@gmail.com>
2019-11-16 16:01:34 -0500

    Updates script1.sh

--- lab4/script1.sh
+++ lab4/script1.sh
@@ -4,15 +4,29 @@
 awk 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", "PID", "USER", "RSS", "COMMAND"; }'
 
 for process in /proc/[0-9]*; do
-	pid=`echo $process | sed -e 's/\/proc\///'`
+    # Retrieve the pid from the directory name
+    pid=${process/\/proc\//}
+    
+    # Retrieve the user from the owner of the directory
 	user=`stat -c %U $process`
-	rss=`grep "VmRSS" $process"/status" | sed -e 's/.*\t *//' ` | sed -e 's/KB//'
-
-	# Check for empty rss values
-	if [[ $rss == "" ]]; then
-		rss="0"
+	
+	# Retrieve the rss
+	rss=`grep 'VmRSS' $process'/status' | sed -e 's/.*\t *//' | sed -e 's/[Kk]B//'`
+	
+	# Check if the rss is empty
+	if [ -z "$rss" ];then
+	    rss=0
 	fi
-
+    
+    # Retrieve the command
 	cmd=`cat $process"/cmdline" | tr '\0' ' '`
+	
+	# Check if the command is empty
+	if [ -z "$cmd" ];then
+	    cmd=`grep 'Name' $process'/status'` | sed -e 's/.*\t *//'
+	    cmd=[$cmd]
+	fi
+	
+	# Print the data
 	awk -v pid="$pid" -v user="$user" -v rss="$rss" -v cmd="$cmd" 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", pid, user, rss, cmd }'
 done
