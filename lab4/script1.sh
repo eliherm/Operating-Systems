022da8f7ac39e0355a046446f509b97346d6ef19
Elikem Hermon <eliherm@gmail.com>
2019-11-15 23:18:07 -0500

    updates script1

--- lab4/script1.sh
+++ lab4/script1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Print the heading
-# awk 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", "PID", "USER", "RSS", "COMMAND"; }'
+awk 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", "PID", "USER", "RSS", "COMMAND"; }'
 
 for process in /proc/[0-9]*; do
 	pid=`echo $process | sed -e 's/\/proc\///'`
-	userId=`grep "Uid" $process"/status" | sed -e 's/.*\t//'`
-	user=`grep '.*:.*:'$userId':.*' "/etc/passwd"`
+	#userId=`grep "Uid" $process"/status" | sed -e 's/.*\t//'`
+	#user=`grep '.*:.*:'$userId':.*' "/etc/passwd"`
 	rss=`grep "VmRSS" $process"/status" | sed -e 's/.*\t//'`
 	cmd=`cat $process"/cmdline" | tr '\0' ' '`
-	stat -c %U $process
-	# awk -v pid="$pid" -v user="$user" -v rss="$rss" -v cmd="$cmd" 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", pid, user, rss, cmd }'
+	user=`stat -c %U $process`
+	awk -v pid="$pid" -v user="$user" -v rss="$rss" -v cmd="$cmd" 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", pid, user, rss, cmd }'
 done
