a7b2b55f3c170806df02c53c53f10b7c8b8c58f7
Elikem Hermon <eliherm@gmail.com>
2019-11-15 23:51:14 -0500

    updates script1

--- lab4/script1.sh
+++ lab4/script1.sh
@@ -7,6 +7,12 @@ for process in /proc/[0-9]*; do
 	pid=`echo $process | sed -e 's/\/proc\///'`
 	user=`stat -c %U $process`
 	rss=`grep "VmRSS" $process"/status" | sed -e 's/.*\t *//' ` | sed -e 's/KB//'
+
+	# Check for empty rss values
+	if [ -z "$rss" ]; then
+		rss=0
+	fi
+	
 	cmd=`cat $process"/cmdline" | tr '\0' ' '`
 	awk -v pid="$pid" -v user="$user" -v rss="$rss" -v cmd="$cmd" 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", pid, user, rss, cmd }'
 done
