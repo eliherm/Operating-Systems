840eac3673c1b3f53a1c27ccd79b5aadb2b54c27
Elikem Hermon <eliherm@gmail.com>
2019-11-15 23:45:43 -0500

    updates script1

--- lab4/script1.sh
+++ lab4/script1.sh
@@ -6,8 +6,7 @@ awk 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", "PID", "USER", "RSS", "COMMAND"; }
 for process in /proc/[0-9]*; do
 	pid=`echo $process | sed -e 's/\/proc\///'`
 	user=`stat -c %U $process`
-	rss=`grep "VmRSS" $process"/status" | sed -e 's/.*\t *//' `
-	
+	rss=`grep "VmRSS" $process"/status" | sed -e 's/.*\t *//' ` | sed -e 's/KB//'
 	cmd=`cat $process"/cmdline" | tr '\0' ' '`
 	awk -v pid="$pid" -v user="$user" -v rss="$rss" -v cmd="$cmd" 'BEGIN { printf "%-10s%-10s%-10s%-10s\n", pid, user, rss, cmd }'
 done
