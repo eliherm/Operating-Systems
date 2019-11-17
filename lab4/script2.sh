17dabbaa776eca216e97ab44b1be05d4218e1f46
Elikem Hermon <eliherm@gmail.com>
2019-11-17 01:22:32 -0500

    Updates script2.sh

--- lab4/script2.sh
+++ lab4/script2.sh
@@ -5,20 +5,22 @@ module_files=""
 other_files=""
 
 for file in `find $1 -type f -name '*.c'`; do
+    file_path=`readlink -f $file`
+
     # Check for the main function
     if grep -q 'int main *(.*)' $file; then
         # Count the number of lines containing printf and fprintf
         printf_count=`grep -wc 'printf' $file`
         fprintf_count=`grep -wc 'fprintf' $file`
-        main_files="${main_files}${file/$1/}: $printf_count,$fprintf_count\n"
+        main_files="${main_files}${file_path}: $printf_count,$fprintf_count\n"
 
     # Check for the init_module function
     elif grep -q 'int init_module *(.*)' $file; then
         # Find lines containing printk
         printk_lines=`grep -wn 'printk' $file | sed -e 's/:.*//' | tr '\n' ','`
-        module_files="${module_files}${file/$1/}: ${printk_lines%,}\n"
+        module_files="${module_files}${file_path}: ${printk_lines%,}\n"
     else
-        other_files="${other_files}${file/$1/}\n"
+        other_files="${other_files}${file_path}\n"
     fi
 done
 
