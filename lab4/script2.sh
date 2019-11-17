2fcc5d0602215e6b74b92605757bd7f1277e722a
Elikem Hermon <eliherm@gmail.com>
2019-11-17 01:01:26 -0500

    Updates script2.sh

--- lab4/script2.sh
+++ lab4/script2.sh
@@ -9,7 +9,7 @@ for file in `find $1 -type f -name '*.c'`; do
     if grep -q 'int main *(.*)' $file; then
         printf_count=`grep -wc 'printf' $file`
         fprintf_count=`grep -wc 'fprintf' $file`
-        main_files+='${file/$1/}: $printf_count, $fprintf_count\n'
+        main_files+='$printf_count, $fprintf_count\n'
 
     # Check for the init_module function
     elif grep -q 'int init_module *(.*)' $file; then
